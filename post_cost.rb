836cbb144849ba003f8b462fd3abf0565013c602
maeda1150 <masaki19770108@gmail.com>
2016-07-31 16:37:37 +0900

    add warning_cost_diff

--- post_cost.rb
+++ post_cost.rb
@@ -23,10 +23,17 @@ slack_config = Util.load_yaml('slack_config.yml', 'aws_config')
 start_time = Time.now
 puts 'Start post cost to slack'
 
+slack = Slack::Incoming::Webhooks.new slack_config['webhook_url']
+warning_cost_diff = slack_config['warning_cost_diff']
+
 aws_configs.each do |config|
   latests = Cost.latest_two(config['profile'])
-  if (latests.first.value != latests.second.value) && (latest.first.created_at > Time.current - 5.minutes)
-    slack = Slack::Incoming::Webhooks.new slack_config['webhook_url']
+  if warning_cost_diff && (latests.first.value - latests.second.value) > warning_cost_diff && (latest.first.created_at > Time.current - 5.minutes)
+    slack.post "@here: "\
+               "profile: #{ latests.first.profile }  "\
+               "cost: #{ latests.first.value.to_s }  "\
+               "time: #{ latests.first.time.to_s }"
+  elsif (latests.first.value != latests.second.value) && (latest.first.created_at > Time.current - 5.minutes)
     slack.post "profile: #{ latests.first.profile }  "\
                "cost: #{ latests.first.value.to_s }  "\
                "time: #{ latests.first.time.to_s }"

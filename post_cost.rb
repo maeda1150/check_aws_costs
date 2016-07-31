a7f3fded1142f9db21f4a7f908b068f85686b8f3
maeda1150 <masaki19770108@gmail.com>
2016-07-31 15:57:12 +0900

    change post slack logic

--- post_cost.rb
+++ post_cost.rb
@@ -1,12 +1,10 @@
-require 'aws-sdk'
-require 'optparse'
 require 'json'
 require 'yaml'
 require 'erb'
 require 'pry'
 require 'active_record'
-require 'slack'
 require 'active_support'
+require 'slack/incoming/webhooks'
 
 Dir.glob("#{File.expand_path('../classes', __FILE__)}/*.rb").each do |file|
   require file
@@ -17,21 +15,9 @@ ActiveRecord::Base.establish_connection(
   database: './database/aws_cost.sqlite'
 )
 
-
 # Set Config
 aws_configs = Util.load_yaml('aws_config.yml', 'aws_config')
 slack_config = Util.load_yaml('slack_config.yml', 'aws_config')
-
-Slack.configure do |config|
-  config.token = slack_config['token']
-end
-
-def post_slack(profile, time, value)
-  Slack.chat_postMessage(
-    text: "profile: #{ profile }  cost: #{ value.to_s }  time: #{ time.to_s }",
-    channel: 'aws_cost'
-  )
-end
 
 # start CloudWatch
 start_time = Time.now
@@ -40,11 +26,10 @@ puts 'Start post cost to slack'
 aws_configs.each do |config|
   latests = Cost.latest_two(config['profile'])
   if (latests.first.value != latests.second.value) && (latest.first.created_at > Time.current - 5.minutes)
-    post_slack(
-      latests.first.profile,
-      latests.first.time,
-      latests.first.value
-    )
+    slack = Slack::Incoming::Webhooks.new slack_config['webhook_url']
+    slack.post "profile: #{ latests.first.profile }  "\
+               "cost: #{ latests.first.value.to_s }  "\
+               "time: #{ latests.first.time.to_s }"
   end
 end
 

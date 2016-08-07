52465b644b041c64dc25dd8cb4ce41fe88ad7d47
maeda1150 <masaki19770108@gmail.com>
2016-08-07 14:52:06 +0900

    post warning

--- post_cost.rb
+++ post_cost.rb
@@ -25,18 +25,26 @@ puts 'Start post cost to slack'
 
 slack = Slack::Incoming::Webhooks.new slack_config['webhook_url']
 warning_cost_diff = slack_config['warning_cost_diff']
+post_str = ''
+warning = false
 
 aws_configs.each do |config|
   latests = Cost.latest_two(config['profile'])
+  if (latests.first.value != latests.second.value) && (latests.first.created_at > Time.current - 5.minutes)
+    post_str << "profile: #{ latests.first.profile }  "\
+                "cost: #{ latests.first.value.to_s }  "\
+                "time: #{ latests.first.time.to_s }\n"
+  end
   if warning_cost_diff && (latests.first.value - latests.second.value) > warning_cost_diff && (latests.first.created_at > Time.current - 5.minutes)
-    slack.post "@here: "\
-               "profile: #{ latests.first.profile }  "\
-               "cost: #{ latests.first.value.to_s }  "\
-               "time: #{ latests.first.time.to_s }"
-  elsif (latests.first.value != latests.second.value) && (latests.first.created_at > Time.current - 5.minutes)
-    slack.post "profile: #{ latests.first.profile }  "\
-               "cost: #{ latests.first.value.to_s }  "\
-               "time: #{ latests.first.time.to_s }"
+    warning = true
+  end
+end
+
+if post_str.present?
+  if warning
+    slack.post "@channel: \n" + post_str, { link_names: 1 }
+  else
+    slack.post post_str
   end
 end
 
